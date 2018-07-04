f60b020dec27445a15c3c99bef7dd5040452ca25
wangzejie <903853302@qq.com>
2018-07-04 16:19:11 +0800

    用vue做的项目

---
+++ vue-project/todoList/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <v-todo></v-todo>
+  </div>
+</template>
+
+<script>
+  import basicCss from './css/basic.css';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'v-todo': todolist,
+  }
+}
+</script>
+
+<style>
+
+</style>
